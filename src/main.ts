1afe27106423c15f0e0ba628dee88982184cf367
YanluPei <peiyanlu@vip.qq.com>
2025-12-02 18:48:33 +0800

    调整托盘创建、检查更新

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import {
   checkSquirrel,
-  createTray,
+  checkUpdate,
   ElectronHost,
   getIconExt,
   inspectElement,
@@ -8,11 +8,9 @@ import {
   isDev,
   onChildWindowOpenUrl,
   showAndFocus,
-  writeLog,
 } from '@peiyanlu/electron-ipc/backend'
-import { app, autoUpdater, BrowserWindow, dialog, globalShortcut, Menu } from 'electron'
+import { app, BrowserWindow, dialog, globalShortcut, Menu } from 'electron'
 import { join } from 'path'
-import { updateElectronApp } from 'update-electron-app'
 import { ElectronSvgHandler } from './electron/IpcHandler'
 
 
@@ -20,9 +18,8 @@ const file = join(__dirname, '..', `renderer/${ MAIN_WINDOW_VITE_NAME }/index.ht
 const frontendURL = MAIN_WINDOW_VITE_DEV_SERVER_URL ?? file
 
 
-const getIcon = (root: string, tray?: boolean) => {
-  return join(root, 'icons', `icon.${ getIconExt(tray) }`)
-}
+const getIcon = (root: string, tray?: boolean) => join(root, 'icons', `icon.${ getIconExt(tray) }`)
+
 const appIcon = getIcon(__dirname)
 const trayIcon = getIcon(isDev ? __dirname : process.resourcesPath, true)
 
@@ -47,16 +44,34 @@ ElectronHost.openMainWindow({
     hideAppMenu: true,
     singleInstance: true,
     devTools: true,
+    backgroundColor: '#141218',
     beforeReady: () => {
       app.commandLine.appendSwitch('log-level', '3')
       app.commandLine.appendSwitch('enable-features', 'GlobalShortcutsPortal')
       
       onChildWindowOpenUrl()
     },
+    tray: {
+      icon: trayIcon,
+      menu: Menu.buildFromTemplate([
+        {
+          label: '打开',
+          click: () => {
+            showAndFocus(ElectronHost.mainWindow)
+          },
+        },
+        {
+          label: '退出',
+          click: () => {
+            ElectronHost.shutdown()
+          },
+        },
+      ]),
+      title: `${ APP_NAME } ${ APP_VERSION }`,
+    },
   })
   .then(async (window) => {
     if (!window) return
-    window.setBackgroundColor('#141218')
     
     if (isDev) {
       globalShortcut.register('CmdOrCtrl+Shift+I', () => {
@@ -64,47 +79,25 @@ ElectronHost.openMainWindow({
       })
     }
     
-    const tray = createTray({
-      window: window,
-      icon: trayIcon,
-      menu: Menu.buildFromTemplate([
-        {
-          label: '打开',
-          click: () => {
-            showAndFocus(window)
-          },
-        },
-        {
-          label: '退出',
-          click: () => {
-            app.exit()
-          },
-        },
-      ]),
-      title: `${ APP_NAME } ${ APP_VERSION }`,
-    })
-    
     IpcHost.addListener('element:focus', async (_evt, data) => {
       await inspectElement(window, data, [ 1, 1 ])
     })
-    
-    
-    if (!isDev) {
-      updateElectronApp({
-        onNotifyUser: async ({ event, ...info }) => {
-          writeLog(`UPDATE: ${ Object.values(info).join(' ') }`)
-          
-          const { response } = await dialog.showMessageBox({
-            icon: trayIcon,
-            title: APP_NAME,
-            message: `${ APP_VERSION } 版本已下载，重启应用后生效`,
-            detail: '取消后，更新仍会在下一次应用启动时生效',
-          })
-          if (0 === response) {
-            tray.enableQuit()
-            autoUpdater.quitAndInstall()
-          }
-        },
-      })
-    }
   })
+
+
+if (!isDev) {
+  checkUpdate({
+    repo: 'peiyanlu/svgoify',
+    onNotifyUser: async ({ done }) => {
+      const { response } = await dialog.showMessageBox({
+        icon: trayIcon,
+        title: APP_NAME,
+        message: '新版本已准备好，确定重启并安装？',
+        detail: '关闭后，更新将在下次启动时应用',
+      })
+      if (0 === response) {
+        done()
+      }
+    },
+  })
+}
